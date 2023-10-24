d35157b9dc5e5c570970e3f8ae0ca88eaf260612
m1l3s3n <141837382+M1l3s3n@users.noreply.github.com>
2023-10-24 09:02:19 +0300

    Плутон, нептун + обновив код

--- site.js
+++ site.js
@@ -1,6 +1,6 @@
 var canvas = document.getElementById("renderCanvas");
 var engine = new BABYLON.Engine(canvas, true);
-var scene, sun, mercury, venus, mars, moon;
+var scene, sun, mercury, venus, mars, moon, neptune, pluto;
 
 var createScene = function () {
     scene = new BABYLON.Scene(engine);
@@ -28,53 +28,88 @@ var createScene = function () {
                                 //СОНЦЕ!!!
     BABYLON.SceneLoader.ImportMesh("", "models/", "sun2.glb", scene, function (meshes) {
         sun = meshes[0];
+        sun.name = "sun";
         sun.position = new BABYLON.Vector3(0, 0, 0);
         sun.scaling = new BABYLON.Vector3(7, 7, 7);
+        createPlanetRotationAnimation(sun, 30);
         //addClickEvent(sun, scene, camera, new BABYLON.Vector3(0, 0, 0));
     });
 
                                 //ВЕНЕРА!!!
     BABYLON.SceneLoader.ImportMesh("", "models/", "venus4.glb", scene, function (meshes) {
         venus = meshes[0];
+        venus.name = "venus";
         venus.position = new BABYLON.Vector3(25, 0, 0);
         venus.scaling = new BABYLON.Vector3(1, 1, 1);
+        createPlanetRotationAnimation(venus, 40);
     });
 
                                 //МЕРКУРІЙ!!!
     BABYLON.SceneLoader.ImportMesh("", "models/", "mercury2.glb", scene, function (meshes) {
         mercury = meshes[0];
+        mercury.name = "mercury";
         mercury.position = new BABYLON.Vector3(15, 0, 0);
         mercury.scaling = new BABYLON.Vector3(0.5, 0.5, 0.5);
+        createPlanetRotationAnimation(mercury, 50);
         //addClickEvent(mercury, scene, camera, mercury.position);
+    });
 
                                 //МАРС!!!
     BABYLON.SceneLoader.ImportMesh("", "models/", "mars2.glb", scene, function (meshes) {
         mars = meshes[0];
+        mars.name = "mars";
         mars.position = new BABYLON.Vector3(35, 0, 0);
         mars.scaling = new BABYLON.Vector3(1, 1, 1);
+        createPlanetRotationAnimation(mars, 35);
     });    
+
                                 //МІСЯЦЬ!!!
-    BABYLON.SceneLoader.ImportMesh("", "models/", "Moon.gltf", scene, function(meshes) {
+    BABYLON.SceneLoader.ImportMesh("", "models/", "Moon.gltf", scene, function (meshes) {
         moon = meshes[0];
+        moon.name = "moon";
         moon.position = new BABYLON.Vector3(40, 0, 0);
         moon.scaling = new BABYLON.Vector3(1, 1, 1);
+        createPlanetRotationAnimation(moon, 25);
     });
 
-// Додати анімацію обертання планети
-        var mercuryRotationAnimation = new BABYLON.Animation(
-        "mercuryRotation",
-        "rotation.y",
-        30,
-        BABYLON.Animation.ANIMATIONTYPE_FLOAT,
-        BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE
-    );                             
-        var mercuryRotationKeys = [];
-        mercuryRotationKeys.push({ frame: 0, value: 0 });
-        mercuryRotationKeys.push({ frame: 100, value: 2 * Math.PI });
-        mercuryRotationAnimation.setKeys(mercuryRotationKeys);
-        mercury.animations = [mercuryRotationAnimation];
-        scene.beginAnimation(mercury, 0, 100, true); // Почати анімацію
+                                //НЕПТУН!!!
+    BABYLON.SceneLoader.ImportMesh("", "models/", "neptune4.glb", scene, function (meshes) {
+        neptune = meshes[0];
+        neptune.name = "neptune";
+        neptune.position = new BABYLON.Vector3(60, 0, 0);
+        neptune.scaling = new BABYLON.Vector3(1, 1, 1);
+        createPlanetRotationAnimation(neptune, 25);
     });
+
+                                //ПЛУТОН!!!
+    BABYLON.SceneLoader.ImportMesh("", "models/", "pluto2.glb", scene, function (meshes) {
+        pluto = meshes[0];
+        pluto.name = "pluto";
+        pluto.position = new BABYLON.Vector3(50, 0, 0);
+        pluto.scaling = new BABYLON.Vector3(1, 1, 1);
+        createPlanetRotationAnimation(pluto, 25);
+    });
+
+
+        // Додати анімацію обертання планет
+        function createPlanetRotationAnimation(planet, speed) {
+            var rotationAnimation = new BABYLON.Animation(
+                planet.name + "Rotation",
+                "rotation.y",
+                speed,
+                BABYLON.Animation.ANIMATIONTYPE_FLOAT,
+                BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE
+            );
+    
+            var rotationKeys = [];
+            rotationKeys.push({ frame: 0, value: 0 });
+            rotationKeys.push({ frame: 100, value: 2 * Math.PI });
+            rotationAnimation.setKeys(rotationKeys);
+            planet.animations = [rotationAnimation];
+            scene.beginAnimation(planet, 0, 100, true); // Почати анімацію
+        }
+
+        
 
                                 //СВІТЛО!!!
     var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 0, 0), scene);
@@ -82,16 +117,16 @@ var createScene = function () {
 
     return scene;
 };
-                                //НЕРОБОЧА ФІГНЯ!!!
-function addClickEvent(mesh, scene, camera, target) {
-    mesh.actionManager = new BABYLON.ActionManager(scene);
-    mesh.actionManager.registerAction(new BABYLON.ExecuteCodeAction(
-        BABYLON.ActionManager.OnPickTrigger,
-        function () {
-            camera.target = target;
-        }
-    ));
-}
+//                                 //НЕРОБОЧА ФІГНЯ!!!
+// function addClickEvent(mesh, scene, camera, target) {
+//     mesh.actionManager = new BABYLON.ActionManager(scene);
+//     mesh.actionManager.registerAction(new BABYLON.ExecuteCodeAction(
+//         BABYLON.ActionManager.OnPickTrigger,
+//         function () {
+//             camera.target = target;
+//         }
+//     ));
+// }
 
 var scene = createScene();
 
