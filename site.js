cf0bd9dd1b7a760c92195dc40822a0650d9d2d09
m1l3s3n <141837382+M1l3s3n@users.noreply.github.com>
2023-10-25 17:23:38 +0300

    видалив все що стосується анімацій

--- site.js
+++ site.js
@@ -28,97 +28,58 @@ var createScene = function () {
                                 //СОНЦЕ!!!
     BABYLON.SceneLoader.ImportMesh("", "models/", "sun.glb", scene, function (meshes) {
         sun = meshes[0];
-        sun.name = "sun";
         sun.position = new BABYLON.Vector3(0, 0, 0);
         sun.scaling = new BABYLON.Vector3(7, 7, 7);
-        createPlanetRotationAnimation(sun, 30);
-        //addClickEvent(sun, scene, camera, new BABYLON.Vector3(0, 0, 0));
     });
 
                                 //ВЕНЕРА!!!
     BABYLON.SceneLoader.ImportMesh("", "models/", "venus.glb", scene, function (meshes) {
         venus = meshes[0];
-        venus.name = "venus";
         venus.position = new BABYLON.Vector3(20, 0, 0);
         venus.scaling = new BABYLON.Vector3(1, 1, 1);
-        createPlanetRotationAnimation(venus, 40);
     });
 
                                 //МЕРКУРІЙ!!!
     BABYLON.SceneLoader.ImportMesh("", "models/", "mercury.glb", scene, function (meshes) {
         mercury = meshes[0];
-        mercury.name = "mercury";
         mercury.position = new BABYLON.Vector3(10, 0, 0);
         mercury.scaling = new BABYLON.Vector3(0.5, 0.5, 0.5);
-        createPlanetRotationAnimation(mercury, 50);
-        //addClickEvent(mercury, scene, camera, mercury.position);
     });
 
                                 //МАРС!!!
     BABYLON.SceneLoader.ImportMesh("", "models/", "mars.glb", scene, function (meshes) {
         mars = meshes[0];
-        mars.name = "mars";
         mars.position = new BABYLON.Vector3(35, 0, 0);
         mars.scaling = new BABYLON.Vector3(1, 1, 1);
-        createPlanetRotationAnimation(mars, 35);
     });    
 
                                 //МІСЯЦЬ!!!
     BABYLON.SceneLoader.ImportMesh("", "models/", "Moon.gltf", scene, function (meshes) {
         moon = meshes[0];
-        moon.name = "moon";
         moon.position = new BABYLON.Vector3(40, 0, 0);
         moon.scaling = new BABYLON.Vector3(1, 1, 1);
-        createPlanetRotationAnimation(moon, 25);
     });
 
                                 //НЕПТУН!!!
     BABYLON.SceneLoader.ImportMesh("", "models/", "neptune.glb", scene, function (meshes) {
         neptune = meshes[0];
-        neptune.name = "neptune";
         neptune.position = new BABYLON.Vector3(100, 0, 0);
         neptune.scaling = new BABYLON.Vector3(1, 1, 1);
-        createPlanetRotationAnimation(neptune, 25);
     });
 
                                 //ПЛУТОН!!!
     BABYLON.SceneLoader.ImportMesh("", "models/", "pluto.glb", scene, function (meshes) {
         pluto = meshes[0];
-        pluto.name = "pluto";
         pluto.position = new BABYLON.Vector3(120, 0, 0);
         pluto.scaling = new BABYLON.Vector3(1, 1, 1);
-        createPlanetRotationAnimation(pluto, 25);
     });
 
 
     BABYLON.SceneLoader.ImportMesh("", "models/", "earth.glb", scene, function (meshes) {
         earth = meshes[0];
-        earth.name = "earth";
         earth.position = new BABYLON.Vector3(27, 0, 0);
         earth.scaling = new BABYLON.Vector3(1, 1, 1);
-        createPlanetRotationAnimation(earth, 25);
     });
-
-
-        // Додати анімацію обертання планет
-        function createPlanetRotationAnimation(planet, speed) {
-            var rotationAnimation = new BABYLON.Animation(
-                planet.name + "Rotation",
-                "rotation.y",
-                speed,
-                BABYLON.Animation.ANIMATIONTYPE_FLOAT,
-                BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE
-            );
-    
-            var rotationKeys = [];
-            rotationKeys.push({ frame: 0, value: 0 });
-            rotationKeys.push({ frame: 100, value: 2 * Math.PI });
-            rotationAnimation.setKeys(rotationKeys);
-            planet.animations = [rotationAnimation];
-            scene.beginAnimation(planet, 0, 100, true); // Почати анімацію
-        }
-
-        
 
                                 //СВІТЛО!!!
     var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 0, 0), scene);
@@ -126,16 +87,8 @@ var createScene = function () {
 
     return scene;
 };
-//                                 //НЕРОБОЧА ФІГНЯ!!!
-// function addClickEvent(mesh, scene, camera, target) {
-//     mesh.actionManager = new BABYLON.ActionManager(scene);
-//     mesh.actionManager.registerAction(new BABYLON.ExecuteCodeAction(
-//         BABYLON.ActionManager.OnPickTrigger,
-//         function () {
-//             camera.target = target;
-//         }
-//     ));
-// }
+
+scene.debugLayer.show();
 
 var scene = createScene();
 
