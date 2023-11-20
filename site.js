2990914d324f8b70cc07b9b705dbd508a648ed1d
m1l3s3n <141837382+M1l3s3n@users.noreply.github.com>
2023-11-20 23:52:04 +0200

    Added star box for Main Page

--- site.js
+++ site.js
@@ -5,7 +5,14 @@ var createScene = function () {
     scene = new BABYLON.Scene(engine);
     scene.clearColor = new BABYLON.Color3(0, 0, 0);
 
-
+    BABYLON.SceneLoader.ImportMesh("", "../models/", "backgroundspace3.glb", scene, function (meshes) {
+        var backgroundspace = meshes[0];
+        backgroundspace.position = new BABYLON.Vector3(0, 0, 0);
+        backgroundspace.scaling = new BABYLON.Vector3(1000, 1000, 1000);
+        var material = new BABYLON.StandardMaterial("backgroundMaterial", scene);
+        material.emissiveTexture = new BABYLON.Texture("", scene);
+        backgroundspace.material = material;
+    });
 
 };
 
