ac61b25fd748aab70e961b2c3856778668bb2503
m1l3s3n <141837382+M1l3s3n@users.noreply.github.com>
2023-11-22 22:34:33 +0200

    Added Scene code, code for camera a scene and light for scene

--- site.js
+++ site.js
@@ -1,10 +1,23 @@
 var canvas = document.getElementById("renderCanvas");
 var engine = new BABYLON.Engine(canvas, true);
+var scene;
 
+var initialCameraPosition = new BABYLON.Vector3(-Math.PI / 6, Math.PI / 2.1, 50);
+
+//Scene
 var createScene = function () {
     scene = new BABYLON.Scene(engine);
     scene.clearColor = new BABYLON.Color3(0, 0, 0);
 
+    //Scene camera
+    var camera = new BABYLON.ArcRotateCamera("Camera", -Math.PI / 6, Math.PI / 3, 1000, BABYLON.Vector3.Zero(), scene);
+    camera.attachControl(canvas, true);
+    camera.lowerRadiusLimit = 45;
+    camera.upperRadiusLimit = 1000;
+    camera.checkCollisions = true;
+    camera.inputs.attached.pointers.buttons = [0];
+
+    //Background sphere(Star box sphere)
     BABYLON.SceneLoader.ImportMesh("", "../models/", "backgroundspace3.glb", scene, function (meshes) {
         var backgroundspace = meshes[0];
         backgroundspace.position = new BABYLON.Vector3(0, 0, 0);
@@ -14,6 +27,11 @@ var createScene = function () {
         backgroundspace.material = material;
     });
 
+    // Light for scene
+    var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 0, 0), scene);
+    light.intensity = 0.25;
+
+    return scene;
 };
 
 engine.runRenderLoop(function () {
