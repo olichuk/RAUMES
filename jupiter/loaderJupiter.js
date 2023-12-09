2af652d3488cb7675632a4cc65a8a5fa005c369d
feveron <veronika.fedyk06@gmail.com>
2023-12-09 21:15:25 +0200

    fixed the box appearing problem

--- jupiter/loaderJupiter.js
+++ jupiter/loaderJupiter.js
@@ -13,3 +13,14 @@ showLoader();
 setTimeout(function() {
     hideLoader();
 }, 3000);
+
+var box = document.querySelector('#box');
+
+function showBox(){
+    box.classList.remove('hidden');
+    box.classList.add('visible')
+}
+
+setTimeout(function(){
+    showBox();
+}, 2000)
